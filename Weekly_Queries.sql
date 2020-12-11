210c48d73f7d548710468ed138a2fa1f18fad875
Grant Culp <culpgrant21@gmail.com>
2020-12-10 19:49:09 -0600

    Update Weekly_Queries.sql

--- Weekly_Queries.sql
+++ Weekly_Queries.sql
@@ -3,30 +3,23 @@
 
 -- TOP 5 Songs Last 7 Days By Time Listened.
 -- QUERY:
-SELECT 
-  st.song_name, 
-  ROUND(SUM(CAST(duration_ms AS decimal)/60000),2) AS min_duration
-FROM 
-  spotify_track AS st
-WHERE 
-  date_time_played > CURRENT_DATE - INTERVAL '7 days'
-GROUP BY 
-  st.song_name
-ORDER BY 
-  min_duration DESC
-LIMIT 
-  5;
+SELECT   st.song_name, 
+         Round(Sum(Cast(duration_ms AS DECIMAL)/60000),2) AS min_duration 
+FROM     spotify_track                                    AS st 
+WHERE    date_time_played > CURRENT_DATE - interval '7 days' 
+GROUP BY st.song_name 
+ORDER BY min_duration DESC limit 5;
+
 -- FUNCTION:
-CREATE FUNCTION function_last_7_days_top_5_songs_duration()
-    RETURNS TABLE (song_name TEXT,
-                   min_duration DECIMAL) LANGUAGE plpgsql 
-AS $$
-BEGIN
-    RETURN QUERY
-    SELECT st.song_name, ROUND(SUM(CAST(duration_ms AS decimal)/60000),2) AS min_duration
-    FROM spotify_track AS st
-    WHERE date_time_played > CURRENT_DATE - INTERVAL '7 days'
-    GROUP BY st.song_name
-    ORDER BY min_duration DESC
-    LIMIT 5;
-END;$$
+CREATE FUNCTION function_last_7_days_top_5_songs_duration() 
+returns TABLE (song_name text, min_duration decimal) language plpgsql AS $$ 
+BEGIN 
+  RETURN query 
+  SELECT   st.song_name, 
+           round(sum(cast(duration_ms AS decimal)/60000),2) AS min_duration 
+  FROM     spotify_track                                    AS st 
+  WHERE    date_time_played > CURRENT_DATE - interval '7 days' 
+  GROUP BY st.song_name 
+  ORDER BY min_duration DESC limit 5; 
+
+end;$$
