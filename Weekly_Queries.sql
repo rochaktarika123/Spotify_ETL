339799bf1aaa94e374e98cf8223dc9660619fc09
Grant Culp <culpgrant21@gmail.com>
2020-12-14 20:01:24 -0600

    Update Weekly_Queries.sql

--- Weekly_Queries.sql
+++ Weekly_Queries.sql
@@ -23,3 +23,19 @@ BEGIN
   ORDER BY min_duration DESC limit 5; 
 
 end;$$
+
+-- Total Time Listened in Last 7 Days (Hours):
+-- Query:
+SELECT ROUND(SUM(CAST (duration_ms AS decimal)/3600000),2) AS total_time_listened_hrs
+FROM spotify_track
+WHERE date_time_played > CURRENT_DATE - INTERVAL '7 days';
+
+-- Function:
+CREATE FUNCTION function_last_7_days_hrs_listened() 
+RETURNS TABLE (total_time_listened_hrs decimal) LANGUAGE plpgsql AS $$ 
+BEGIN 
+  RETURN query 
+  SELECT   ROUND(SUM(CAST (st.duration_ms AS decimal)/3600000),2) AS total_time_listened_hrs 
+  FROM     spotify_schema.spotify_track AS st 
+  WHERE    date_time_played > CURRENT_DATE - INTERVAL '7 days';
+end;$$ 
